37058c124ebced743243cb7d98227095f3d67de5
andresolmos <80769478+andresolmos@users.noreply.github.com>
2024-11-22 09:59:14 -0500

    *

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'u873923035_moodle';
+$CFG->dbuser    = 'u873923035_moodle';
+$CFG->dbpass    = 'Moodle2784.';
+$CFG->prefix    = 'edu_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'http://edugestion.scordesign.com';
+$CFG->dataroot  = '/home/u873923035/domains/scordesign.com/public_html/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
